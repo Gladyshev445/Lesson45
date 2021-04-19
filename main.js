7dfc61b83a71f2235a9be1638d8e8ba425ad6a58
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-04-19 10:08:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// function showMessege (){
+//     alert("Привет Ярослав")
+// }
+// showMessege();
+
+// function showMessege(){
+//     let a = "Diss"
+//     alert(a)
+// }
+// showMessege();
+
+// function showMessege(){
+//     let p = prompt("Как тебя зовут?")
+//     alert("Тебя зовут" + " " + p)
+// }
+// showMessege();
+
+// let Myname = prompt("Как тебя зовут?")
+// function showMessege(){
+//     let a = "Привет" + " " + Myname
+//     alert(a)
+// }
+// showMessege();
+
+// function showMessege(from, text){
+//     alert( from + "получит" + " " + text)
+
+// }
+// showMessege("Yarok" + " " , "diploma");
+
+// function showMessege (a , b){
+//     alert(a + " " + "надевает" + " " + b)
+// }
+// let a = prompt("Как вас зовут?")
+//     b = prompt("Что наденете?")
+// showMessege(a , b);
+
+let a = Number(prompt("a = "))
+    b = Number(prompt("b = "))
+function sum (a , b){
+    return a + b
+}
+let c =  sum(10, 0.006)
+alert(c)
